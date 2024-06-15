b4bf58dff3e167e062d9ba889a2df14787433952
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-16 00:03:13 +0530

    Added the main file

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client/edge'
+import { sign, verify, decode, jwt } from 'hono/jwt'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
+  }
+}>()
+
+
+app.route("/api/v1/user", userRouter);
+app.route ("/api/v1/blog", blogRouter);
+
+
+
+
+export default app

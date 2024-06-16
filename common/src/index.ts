76dd6af258b532512ad9808f35ea3fa7f306d45a
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-16 20:02:10 +0530

    Added the common directory for validation

---
+++ common/src/index.ts
@@ -0,0 +1,39 @@
+import z from "zod"
+
+export const signupSchema = z.object({
+    email: z.string().email({
+        message: "Invalid email format"
+    }),
+    password: z.string().min(8, {
+        message: "Passwords should be 8 characters long"
+    }),
+    name: z.string()
+})
+
+export type signupSchema = z.infer<typeof signupSchema>
+
+export const signinSchema = z.object({
+    email: z.string().email({
+        message: "Invalid email format"
+    }),
+    password : z.string().min(8, {
+        message: "Password should be 8 characters long"
+    })
+})
+
+export type SignInSchema = z.infer<typeof signinSchema>
+
+export const blogpostSchema = z.object({
+    title: z.string(),
+    content: z.string()
+})
+
+export type BlogPostSchema = z.infer<typeof blogpostSchema>
+
+export const blogupdateSchema = z.object({
+    id: z.number(),
+    title: z.string().optional(),
+    content: z.string().optional()
+})
+
+export type BlogUpdateSchema = z.infer<typeof blogupdateSchema>
